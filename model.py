c5ac305ff33f26c20520da93e9e997bbb42e5264
Hackbright Student <user@chromebox08.(none)>
2013-10-29 18:02:20 -0700

    Working with movies and ratings, still do not have adding judgements

--- model.py
+++ model.py
@@ -1,13 +1,47 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
+import datetime
+
+ENGINE = create_engine("sqlite:///ratings.db", echo=False)
+session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
 
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key=True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
 
-### End class declarations
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=True)
+    released_at = Column(DateTime(), nullable=True)
+    imdb_url = Column(String(64), nullable=True)
+
+    #rating = relationship("Rating", backref=backref("movies", order_by=id))
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    rating = Column(Integer)
+    timestamp = Column(String(15), nullable=True)
+
+    user = relationship("User", backref=backref("ratings", order_by=id))
+    movie = relationship("Movie", backref=backref("ratings"))
+## End class declarations
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,22 +1,47 @@
 import model
 import csv
+import datetime
 
 def load_users(session):
     # use u.user
-    pass
+    with open("seed_data/u.user", "rb") as csvfile:
+        users = csv.reader(csvfile, delimiter= '|')
+        for user in users:
+            new_user = model.User(id=user[0], age=user[1], zipcode=user[4])
+            session.add(new_user)
+    session.commit()
 
 def load_movies(session):
     # use u.item
-    pass
+    with open("seed_data/u.item", "rb") as csvfile:
+        movies = csv.reader(csvfile, delimiter= '|')
+        for movie in movies:
+            try:
+                date = str(datetime.datetime.strptime(movie[2], '%d-%b-%Y'))
+                split_date = date.split()[0].split("-")
+                release_date = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+                new_movie = model.Movie(id=movie[0], name=movie[1].decode("latin-1"), released_at=release_date, imdb_url=movie[4])
+                session.add(new_movie)
+            except:
+                continue
+    session.commit()
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open("seed_data/u.data", "rb") as csvfile:
+        ratings = csv.reader(csvfile, delimiter="\t")
+        for rating in ratings:
+            date = str(datetime.datetime.fromtimestamp(float(rating[3]))).split()[0]
+            new_rating = model.Rating(user_id=rating[0], movie_id=rating[1], rating=rating[2], timestamp=date)
+            session.add(new_rating)
+    session.commit()
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(s)
+    load_movies(s)
+    load_ratings(s)
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
