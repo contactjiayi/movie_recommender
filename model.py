6a318f649fbe04a723554a941b664649a8b19057
Hackbright Student <user@chromebox08.(none)>
2013-10-30 18:13:27 -0700

    All functionalities, but not working

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 import datetime
+import correlation
 
 ENGINE = create_engine("sqlite:///ratings.db", echo=False)
 session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
@@ -19,6 +20,36 @@ class User(Base):
     password = Column(String(64), nullable=True)
     age = Column(Integer, nullable=True)
     zipcode = Column(String(15), nullable=True)
+
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        other_users = [ r.user for r in other_ratings ]
+        similarities = [ (self.similarity(r.user), r, r.user) for r in other_ratings ]
+        similarities.sort(reverse = True)
+        top_user = similarities[0]
+        matched_rating = None
+        for rating in other_ratings:
+            if rating.user_id == top_user[2].id:
+                matched_rating = rating
+                break
+        return matched_rating.rating * top_user[0]
 
 class Movie(Base):
     __tablename__ = "movies"
